181b31d067d462e4ce1fd5afa97f1f8d489d626d
herman030103-eng <german030103@gmail.com>
2026-02-06 01:34:55 +0100

    notation

--- hpc_git/aixelo/CGCNN/run_odac_co2.sh
+++ hpc_git/aixelo/CGCNN/run_odac_co2.sh
@@ -12,46 +12,29 @@
 unset SLURM_EXPORT_ENV
 set -euo pipefail
 
-
-# -------------------------------
-# Пути
-# -------------------------------
 PROJECT_ROOT="$SLURM_SUBMIT_DIR"
 CODE_DIR="$PROJECT_ROOT/code"
-# Это путь к АРХИВУ
 DATASET_ARCHIVE="/home/vault/b192aa/b192aa41/datasets/odac_co2.tar"
 RESULTS_DIR="$PROJECT_ROOT/results/cgcnn/job_${SLURM_JOB_ID}"
 CONTAINER="$PROJECT_ROOT/../container/cont.sif"
 
-# Это путь куда распакуем
 TMP_DATA="$TMPDIR/odac_data"
 
 mkdir -p "$TMP_DATA" "$RESULTS_DIR"
 
-# -------------------------------
-# Распаковка
-# -------------------------------
-echo "Распаковка данных из $DATASET_ARCHIVE в $TMP_DATA..."
+echo "Getting data from $DATASET_ARCHIVE to $TMP_DATA..."
 tar xf "$DATASET_ARCHIVE" -C "$TMP_DATA"
 
-# ПРОВЕРКА СТРУКТУРЫ
-# Если архив содержит папку 'dataset', то путь правильный.
-# Если файлы лежат сразу в корне архива, то DATA_ROOT=$TMP_DATA
 if [ -d "$TMP_DATA/dataset" ]; then
     DATA_ROOT="$TMP_DATA/dataset"
 else
-    # Предполагаем, что распаковалось прямо в TMP_DATA
     DATA_ROOT="$TMP_DATA"
 fi
 
-echo "Корневая папка данных: $DATA_ROOT"
+echo "Data root: $DATA_ROOT"
 
-# -------------------------------
-# Запуск
-# -------------------------------
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-# ИСПРАВЛЕНИЕ: Монтируем DATA_ROOT, а не архив
 srun apptainer exec --nv \
     -B "$CODE_DIR:/workspace/code" \
     -B "$DATA_ROOT:/workspace/dataset" \
